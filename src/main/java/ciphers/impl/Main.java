f06fc220ab5a22a33f90514985cc03600843cf33
PKondel <renevoir@gmail.com>
2020-02-15 13:28:51 +0100

    main

---
+++ src/main/java/ciphers/impl/Main.java
@@ -0,0 +1,18 @@
+package ciphers.impl;
+
+public class Main {
+    public static void main(String[] args) {
+        String mama = "mama";
+        char[] lettersInMamaWord = mama.toCharArray();
+
+        for (char letter : lettersInMamaWord) {
+            int newLetterNumber = letter + 3;
+            System.out.print((char) newLetterNumber);
+        }
+        System.out.println();
+
+        CesarCipher text = new CesarCipher();
+        System.out.println(text.encode("ABCabc123XYZxyz"));
+        System.out.println(text.decode("ABCabc123XYZxyz"));
+    }
+}
